5b37efe0921dd8c5105aa9e40a30972f60eb3fba
hasbif <hasbisenpai@gmail.com>
2020-04-27 22:51:59 +0700

    change to list players and search player

--- 01-class-components/src/components/Page.js
+++ 01-class-components/src/components/Page.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import Teams from './Teams'
+// import Teams from './Teams'
+import SimpleTable from './Table'
 
 
 class Page extends Component {
     constructor() {
         super()
         this.state = {
-            teams: []
+            players: [],
+            search: null,
+            searchPlayer: null,
         }
     }
 
     componentDidMount() {
-        fetch("https://free-nba.p.rapidapi.com/teams?page=0", {
+
+        fetch("https://free-nba.p.rapidapi.com/players?page=0&per_page=50", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "free-nba.p.rapidapi.com",
@@ -21,20 +25,109 @@ class Page extends Component {
             .then(response => {
                 return response.json()
             })
-            .then(data => {
-                console.log(data);
-                this.setState({ teams: data.data })
+            .then(response => {
+                console.log(response, 'sdasaojisad')
+                console.log(response.data, 'sdasaojiasddddddasdsad')
+                this.setState({ players: response.data })
             })
             .catch(err => {
                 console.log(err);
             });
+        // fetch("https://free-nba.p.rapidapi.com/teams?page=0", {
+        //     "method": "GET",
+        //     "headers": {
+        //         "x-rapidapi-host": "free-nba.p.rapidapi.com",
+        //         "x-rapidapi-key": "21eedf4d07msha5d52bf1a701aa8p19733bjsn8fa2554194df"
+        //     }
+        // })
+        //     .then(response => {
+        //         return response.json()
+        //     })
+        //     .then(data => {
+        //         // console.log(data);
+        //         this.setState({ teams: data.data })
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+
+
+        // fetch("https://api-nba-v1.p.rapidapi.com/seasons/", {
+        //     "method": "GET",
+        //     "headers": {
+        //         "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+        //         "x-rapidapi-key": "21eedf4d07msha5d52bf1a701aa8p19733bjsn8fa2554194df"
+        //     }
+        // })
+        //     .then(response => {
+        //         console.log(response);
+        //         return response.json()
+        //     })
+        //     .then(response => {
+        //         console.log(response);
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+    }
+
+    searchPlayer = (e) => {
+        e.preventDefault()
+        console.log(e.target.value)
+        console.log(this.state.search)
+
+        fetch("https://free-nba.p.rapidapi.com/players?page=0&per_page=25&search=" + this.state.search, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "free-nba.p.rapidapi.com",
+                "x-rapidapi-key": "21eedf4d07msha5d52bf1a701aa8p19733bjsn8fa2554194df"
+            }
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(response => {
+                console.log(response, 'playerssdasaojisad')
+                console.log(response.data, 'playessdasaojiasddddddasdsad')
+                this.setState({ searchPlayer: response.data })
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+
+
+        // this.setState({
+        //     search: null
+        // })
+    }
+
+    clearSearch = () => {
+        this.setState({
+            searchPlayer: null
+        })
+        // this.setState({
+        //     search: null
+        // })
+    }
+
+    handleInput = (e) => {
+        this.setState({
+            search: e.target.value.trim().replace(/ /g, "%20")
+        })
     }
 
     render() {
         return <div>
-            <h1>NBA Info</h1>
-            <Teams data={this.state.teams} />
-            hi from component
+            <h1>NBA Player Info</h1>
+
+            <form>
+                <input id="searchForm" placeholder="Search team..." onChange={this.handleInput}></input>
+                <button onClick={this.searchPlayer}>Search</button>
+            </form>
+            <button onClick={this.clearSearch}>Clear</button>
+            {this.state.searchPlayer ? <SimpleTable data={this.state.searchPlayer}></SimpleTable> : <SimpleTable data={this.state.players}></SimpleTable>}
+
         </div>
     }
 }
---
+++ 01-class-components/src/components/Table.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+
+export default function SimpleTable(props) {
+    const classes = useStyles();
+    const { data } = props
+
+
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>First Name</TableCell>
+                        <TableCell align="right">Last Name</TableCell>
+                        <TableCell align="right">Position</TableCell>
+                        <TableCell align="right">Team</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map((player, idx) => (
+                        <TableRow key={idx}>
+                            <TableCell component="th" scope="row">
+                                {player.last_name}
+                            </TableCell>
+                            <TableCell align="right">{player.first_name}</TableCell>
+                            <TableCell align="right">{player.position === "" ? 'N/A' : player.position}</TableCell>
+                            <TableCell align="right">{player.team.abbreviation}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
