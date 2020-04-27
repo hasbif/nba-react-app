396cce9a725e56f1088fcc1e907563e7721afe90
hasbif <hasbisenpai@gmail.com>
2020-04-27 16:35:36 +0700

    initialize and set components

---
+++ 01-class-components/src/components/Page.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class Page extends Component {
+    constructor() {
+        super()
+        this.state = {
+            teams: []
+        }
+    }
+    render() {
+        return <h1>frin cinoibeasd</h1>
+    }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default Page;
